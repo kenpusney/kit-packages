691ff49401558335c93c11617c91101603415462
Kimmy Leo <kenpusney@gmail.com>
2013-03-21 01:13:28 +0800

    add some res.

--- lib/debug.sh
+++ lib/debug.sh
@@ -1,4 +1,7 @@
 #
 # Debug Library.
 # Ref. http://coolshell.cn/articles/1379.html
+# Ref. http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_02_03.html
+# Ref. http://www.cyberciti.biz/tips/debugging-shell-script.html
+# Ref. http://wiki.bash-hackers.org/scripting/debuggingtips
 #
