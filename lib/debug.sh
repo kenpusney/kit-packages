1809015ebae49964dc32601ac30f8e941cfc732e
Kim Leo <kenpusney@gmail.com>
2013-06-09 21:40:40 +0800

    cleanning

--- lib/debug.sh
+++
@@ -1,8 +0,0 @@
-#
-# Debug Library.
-# Ref. http://coolshell.cn/articles/1379.html
-# Ref. http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_02_03.html
-# Ref. http://www.cyberciti.biz/tips/debugging-shell-script.html
-# Ref. http://wiki.bash-hackers.org/scripting/debuggingtips
-# Ref. http://www.linuxtopia.org/online_books/advanced_bash_scripting_guide/debugging.html
-#
--- lib/net.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-##
-## kit net lib
-## @author : Kimmy Leo
-## @require : `curl` -> '7.27.0'
-##
-
-load_lib "error"
-
